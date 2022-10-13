569869d300f206fc7dcc69a0f2ef61d1b17c9152
Rabia Günbaş <rabiagnbs@gmail.com>
2022-10-14 00:08:32 +0300

    Initial commit

---
+++ src/not_ortalamasi_java.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args) {
+        float a,b,c,d;
+        float toplama,ortalama;
+        System.out.print("Fizik Notunuzu Giriniz.");
+        Scanner input=new Scanner(System.in);
+        a=input.nextFloat();
+
+        System.out.println("Matematik Notunuzu Giriniz.");
+        b=input.nextFloat();
+
+        System.out.println("Kimya Notunuzu Giriniz");
+        c=input.nextFloat();
+
+        System.out.println("Fizik Notunuzu Giriniz.");
+        d=input.nextFloat();
+
+        toplama=a+b+c+d;
+        ortalama=toplama/4;
+        System.out.println("Notlarin ortlamasi:");
+        System.out.print(ortalama);
+    }
+}
