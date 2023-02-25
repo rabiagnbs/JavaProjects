12ec6ddc86237b232e3fbef8567ecc2518604ed0
rabiagnbs <rabiagnbs@gmail.com>
2023-02-25 22:13:14 +0300

    java_projelerim

---
+++ src/DIZILER/YildizBHarfi.java
@@ -0,0 +1,26 @@
+package DIZILER;
+
+public class YildizBHarfi {
+    public static void main(String[] args) {
+        String[][] letter=new String[7][4];
+        for(int i=0; i<letter.length; i++){
+            for(int j=0; j<letter[i].length; j++){
+                if(i==3 || i==0 || i==6){
+                     letter[i][j] = " * ";
+                }
+                else if(j==0 || j==3){
+                     letter[i][j]= " * ";
+                }
+                else{
+                    letter[i][j]= "   ";
+                }
+            }
+        }
+        for(String[] row: letter){
+            for(String col: row){
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+    }
+}
