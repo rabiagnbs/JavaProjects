95ea056465ebcc7ce7014beb54ec4f492028c7c4
rabiagnbs <rabiagnbs@gmail.com>
2023-03-08 12:14:50 +0300

    java_projelerim

---
+++ src/SayiTahminOyunu.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+
+
+
+public class SayiTahminOyunu {
+
+    public static boolean TuttuMu(int sayi, int number){
+        return sayi==number;
+    }
+
+
+    public static void main(String[] args) {
+           Random rand=new Random();
+           int number=rand.nextInt(10);
+
+           System.out.println("lütfen bir sayi giriniz.");
+           Scanner input=new Scanner(System.in);
+
+
+           int sonuc=0;
+           boolean result =false;
+
+           while(sonuc != -1){
+
+               int sayi=input.nextInt();
+               input.nextLine();
+               result =TuttuMu(sayi,number);
+
+               if( result==true){
+                   System.out.println("Sayiyi dogru tahmin ettiniz. Tebrikler!");
+               }
+               else {
+                   System.out.println("Sayiyi dogru tahmin edemediniz. Lütfen tekrar deneyiniz :(");
+               }
+               System.out.println("Tahmin oyunundan cikmak icin lutfen -1'e basiniz.");
+               sonuc=input.nextInt();
+
+
+           }
+           System.out.println("Oyundan cikiliyor...");
+    }
+}
